9b192a5f4a05458233ea102158d520ffbb319a4d
Nicolas Nuytten <nicolas.nuytten@student.howest.be>
2019-11-11 20:31:48 +0100

    Small updates to style

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    require(`stylelint`),
-    require(`postcss-reporter`)({clearReportedMessages: true}),
-    require(`postcss-will-change`),
-    require(`postcss-import`),
-    require(`postcss-cssnext`),
-  ],
-};
